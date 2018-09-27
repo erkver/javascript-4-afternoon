ca64358c38aa68803aaf7a19d29479736dd03c55
Eric Vere <erkver250@gmail.com>
2018-09-27 14:34:41 -0500

    completed javascript-4-afternoon

--- closures.js
+++ closures.js
@@ -23,13 +23,14 @@ function outer() {
 */
   
 // Code Here
+const inner = outer();
 
 
 
 //Once you do that, invoke inner.
 
 //Code Here
-
+inner();
 
 
 ////////// PROBLEM 2 //////////
@@ -52,7 +53,8 @@ function callFriend(name) {
 */
 
 //Code Here
-
+const callJake = callFriend('Jake');
+callJake('435-555-9248');
 
 
 ////////// PROBLEM 3 //////////
@@ -62,15 +64,20 @@ function callFriend(name) {
 */
 
 //Code Here
-
+function makeCounter() {
+  let num = 0;
+  return function() {
+   return num += 1;
+  }
+}
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -87,9 +94,14 @@ function callFriend(name) {
 
 function counterFactory(value) {
   // Code here.
-
+  
   return {
-
+    inc() {
+      return value += 1;
+    },
+    dec() {
+      return value -= 1;
+    }
   };
 }
 
@@ -113,9 +125,11 @@ function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
-
+  function message() {
+    return `${welcomeText} ${firstname} ${lastname}.`;
+  }
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -144,8 +158,13 @@ var module = (function() {
   // outside our lexical scope
   return {
     // Code here.
+    publicMethod() {
+      return privateMethod();
+    }
   };
 })();
+
+module.publicMethod();
 
 
 
@@ -163,6 +182,12 @@ function secretNumber() {
 
   return {
     // Code here
+    addToSecret(addNum) {
+      return secret += addNum;
+    },
+    takeAwayFromSecret(subNum) {
+      return secret -= subNum;
+    }
   };
 }
 
@@ -188,9 +213,12 @@ function secretNumber() {
 
 function timeOutCounter() {
   for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
-      console.log(i);
-    }, i * 1000);
+    log(i);
   }
+  function log(i) {
+    setTimeout(function () {
+    console.log(i);
+    }, i * 1000)};
 }
+ 
 timeOutCounter();
